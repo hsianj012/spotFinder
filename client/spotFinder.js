4df7d1bf66970bc54c48da670b180de58ed7668e
Judy Hsiang <jhsiang514@gmail.com>
2014-01-10 22:53:31 -0500

    attempt at adding db

--- client/spotFinder.js
+++ client/spotFinder.js
@@ -1,3 +1,6 @@
+Lists = new Meteor.Collection("lists");
+
+// ------MAPS--------//
 Template.maps.rendered = function(){
   var mapOptions = {
     zoom: 16,
@@ -14,4 +17,27 @@ Template.filters-box.events({
     if (typeof console !== 'undefined')
       console.log("You pressed the button");
   }
+});
+
+Template.login.events({
+
+  'submit #login-form' : function(e, t){
+    e.preventDefault();
+    // retrieve the input field values
+    var email = t.find('#login-email').value, password = t.find('#login-password').value;
+
+      // Trim and validate your fields here.... 
+
+      // If validation passes, supply the appropriate fields to the
+      // Meteor.loginWithPassword() function.
+      Meteor.loginWithPassword(email, password, function(err){
+      if (err){}
+        // The user might not have been found, or their passwword
+        // could be incorrect. Inform the user that their
+        // login attempt has failed. 
+      else{}
+        // The user has been logged in.
+    });
+       return false; 
+    }
 });
