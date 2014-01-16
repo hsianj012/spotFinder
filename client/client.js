5cc336f661b86b317663b63ea736a68c1a3d86cd
Judy Hsiang <jhsiang514@gmail.com>
2014-01-16 17:56:51 -0500

    added search box; but it's not working in meteor :(

--- client/client.js
+++ client/client.js
@@ -3,13 +3,61 @@ Meteor.startup(function(){
 });
 
 Template.maps.rendered = function(){
+  var markers = [];
   var mapOptions = {
     zoom: 16,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions); 
-      map.setCenter(new google.maps.LatLng( 42.359, -71.093 ));
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: new google.maps.LatLng(42.359, -71.093)
+  };
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions)
+
+  var input = document.getElementById('pac-input');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox(input);
+
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
+  });
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 
   var garages = new google.maps.FusionTablesLayer({
     query: {
@@ -23,6 +71,8 @@ Template.maps.rendered = function(){
 	});
 };
 
+
+
 Template.filtersBoxUser.events({
 	'change .hasSub' : function(){
 		var elems = document.getElementsByClassName('subfilter');
@@ -35,5 +85,12 @@ Template.filtersBoxUser.events({
 Template.filtersBoxNonUser.events({
 	'click .checkboxOverlay' : function(){
 		alert("hello");
+	},
+
+	'change .hasSub' : function(){
+		var elems = document.getElementsByClassName('subfilter');
+		for (var i = 0; i < elems.length; i++) {
+			elems[i].disabled = !elems[i].disabled;
+		};
 	}
 });
